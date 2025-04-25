86743d0add3a95479f1f74e677fa77b3308bf2e6
Fabian <hermanoslorenzop2@gmail.com>
2025-04-24 20:29:58 -0400

    Tarea3

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def Cambio_Divisa(Monto, tasa_cambio):
+    return Monto / tasa_cambio
+
+Monto = float(input("Ingresa la cantidad de su moneda: "))
+Ratio = float(input("Ingresa atio de conversion: "))
+
+MontoConvertido = str(Cambio_Divisa(Monto,Ratio))
+
+
+print("Ahora tiene" + MontoConvertido + " de la moneda local")
